a59633bc31a02e95e5c77af6158ef3e491c44d0f
Nico Team <148757090+NicoDaoMoney@users.noreply.github.com>
2023-11-28 19:14:39 +0800

    Update IconToken.tsx

--- apps/orne-dapp/src/components/ui/IconToken.tsx
+++ apps/orne-dapp/src/components/ui/IconToken.tsx
@@ -1,6 +1,6 @@
 import astro from '~/assets/tokens/astro.png';
 import luna from '~/assets/tokens/luna.png';
-import orne from '~/assets/tokens/orne.png';
+import orne from '~/assets/tokens/seul.png';
 import { Token } from '~/utils/constants';
 
 type IconTokenProps = {
